092e7c1fde6c1f6a716278d5358e13f775374a87
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-11-23 21:14:41 +0530

    sub dir

--- tut01/src/main.rs
+++ tut01/src/main.rs
@@ -31,7 +31,37 @@ fn variable(){
     let add_str = str1.to_owned() + str2;
 
 
-    
+    println!("num1: {}", num1);
+    println!("num2: {}", num2);
+    println!("str1: {}", str1);
+    println!("str2: {}", str2);
+    println!("char1: {}", char1);
+    println!("char2: {}", char2);
+    println!("bool1: {}", bool1);
+    println!("bool2: {}", bool2);
+    println!("my_name: {}", my_name);
+    my_name = "Sameer Bagwan";
+    println!("mutable my_name :{:?}", my_name);
+    println!("num_arr1: {:?}", num_arr1);
+    println!("num_arr2: {:?}", num_arr2);
+    println!("str_arr1: {:?}", str_arr1);
+    println!("str_arr2: {:?}", str_arr2);
+    println!("tuple_1: {:?}", tuple_1);
+    println!("add_num: {}", add_num);
+    println!("add_str: {}", add_str);
+
+    let k: Option<i32> = Some(5); // Optional integer with Some variant
+let l: Option<f64> = None; // Optional float with None variant
+
+enum MyError {
+    CustomError,
+}
+
+fn example_function() -> Result<i32, MyError> {
+    // Function returning Result with an integer or custom error
+    // Some(42) can be Ok(42) as well
+    Ok(42)
+}
 
 
 }
