0e0196590f65db156ab3575378b9192c42c9e29e
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-11-25 20:25:46 +0530

    tut02

--- tut02/src/main.rs
+++ tut02/src/main.rs
@@ -1,15 +1,19 @@
 use std::io;
 
-
-fn main() {
-    let x: i32 = 59;
-    println!("x is : {:?}", x);
-
-    input_output()
+fn read_input() -> String {
+    let mut input = String::new();
+    println!("Enter a message:");
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+    input.trim().to_string()
 }
 
+fn output_value(value: &str) {
+    println!("Input value: {}", value);
+}
 
-fn input_output(){
-    let mut input = String::new();
-    io::stdin().read_line(&mut input);
+fn main() {
+    let t = read_input();
+    output_value(&t);
 }
