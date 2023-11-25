b524340b44992c720770e29d279565017f34a1f7
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-11-25 20:23:27 +0530

    tut01

--- tut02/src/main.rs
+++ tut02/src/main.rs
@@ -1,4 +1,15 @@
+use std::io;
+
+
 fn main() {
     let x: i32 = 59;
-    println!("x is : {:?}", x)
+    println!("x is : {:?}", x);
+
+    input_output()
+}
+
+
+fn input_output(){
+    let mut input = String::new();
+    io::stdin().read_line(&mut input);
 }
