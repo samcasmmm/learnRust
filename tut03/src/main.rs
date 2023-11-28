373fd8a7291d82659a1865bc3a3860a0d03c5d0d
Sameer Bagwan <sameerbagwan132@gmail.com>
2023-11-28 22:21:10 +0530

    ..

---
+++ tut03/src/main.rs
@@ -0,0 +1,18 @@
+// @ Arithmetic and Type Casting
+fn main() {
+    println!("----------------------");
+    // let x: u8 = 255; // 0 - 255
+    // let y: u8 = 10; // 0 - 255
+
+    // let z = x / y;
+
+    let x = 50;
+    let y = 50;
+    let add = x + y;
+    let sub = x - y;
+    let multi = x * y;
+
+    println!("{}", add);
+    println!("{}", sub);
+    println!("{}", multi);
+}
