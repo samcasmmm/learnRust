31a079ff0dacbbdab48af0bb8e04b27a2c228d2a
Sameer Bagwan <87710112+samcasmmm@users.noreply.github.com>
2023-12-15 20:12:24 +0530

    Create formatting.rs

---
+++ formatting.rs
@@ -0,0 +1,9 @@
+// Single Placeholder
+println!("{}", 1);
+
+// Multiple Placeholder
+println!("{} {}", 1, 3);
+
+// Positional Arguments
+println!("{0} is {1} {2}, also {0} is a {3} programming language", "Rust", "cool", "language", "safe");
+
