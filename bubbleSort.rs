8f91e4dd664f4013c87ba81803812dfed0d51630
Sameer Bagwan <87710112+samcasmmm@users.noreply.github.com>
2023-12-17 21:08:44 +0530

    Create bubbleSort.rs

---
+++ bubbleSort.rs
@@ -0,0 +1,23 @@
+fn main() {
+    let mut sortable: [i32; 5] = [5, 8, 2, 7, 6];
+    let length = sortable.len();
+    let mut swapped = true;
+
+    print_array(&sortable);
+
+    while swapped {
+        swapped = false;
+        for i in 1..length {
+            let previous_element = sortable[i - 1];
+            let current_element = sortable[i];
+
+            if previous_element > current_element {
+                sortable.swap(i - 1, i);
+                swapped = true;
+            }
+        }
+    }
+
+    print_array(&sortable);
+}
+
