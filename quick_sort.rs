6e07d781bdcc32a16bc83b7cb3e58c07689c9894
Sameer Bagwan <87710112+samcasmmm@users.noreply.github.com>
2023-12-20 19:30:20 +0530

    Update quick_sort.rs

--- quick_sort.rs
+++ quick_sort.rs
@@ -25,4 +25,26 @@ fn _quick_sort(arr: &mut [T], low: isize, high: isize) {
         _quick_sort(arr, p + 1, high);
     }
 }
+fn partition(arr: &mut [T], low: isize, high: isize) -> isize {
+    let pivot = high as usize;
+    let mut store_index = low - 1;
+    let mut last_index = high;
 
+    loop {
+        store_index += 1;
+        while arr[store_index as usize] < arr[pivot] {
+            store_index += 1;
+        }
+        last_index -= 1;
+        while last_index >= 0 && arr[last_index as usize] > arr[pivot] {
+            last_index -= 1;
+        }
+        if store_index >= last_index {
+            break;
+        } else {
+            arr.swap(store_index as usize, last_index as usize);
+        }
+    }
+    arr.swap(store_index as usize, pivot as usize);
+    store_index
+}
