aa7acce64b000aad59ac4821107113fe435e05eb
Sameer Bagwan <87710112+samcasmmm@users.noreply.github.com>
2023-12-20 19:30:07 +0530

    Update quick_sort.rs

--- quick_sort.rs
+++ quick_sort.rs
@@ -1,1 +1,28 @@
 
+fn main() {
+    println!("Sort numbers ascending");
+    let mut numbers = [4, 65, 2, -31, 0, 99, 2, 83, 782, 1];
+    println!("Before: {:?}", numbers);
+    quick_sort(&mut numbers);
+    println!("After:  {:?}\n", numbers);
+
+    println!("Sort strings alphabetically");
+    let mut strings = ["beach", "hotel", "airplane", "car", "house", "art"];
+    println!("Before: {:?}", strings);
+    quick_sort(&mut strings);
+    println!("After:  {:?}\n", strings);
+}
+
+pub fn quick_sort(arr: &mut [T]) {
+    let len = arr.len();
+    _quick_sort(arr, 0, (len - 1) as isize);
+}
+
+fn _quick_sort(arr: &mut [T], low: isize, high: isize) {
+    if low < high {
+        let p = partition(arr, low, high);
+        _quick_sort(arr, low, p - 1);
+        _quick_sort(arr, p + 1, high);
+    }
+}
+
