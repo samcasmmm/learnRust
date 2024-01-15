932c2e6855801b82fd320f6673802b2e7d87fabc
Sameer Bagwan <87710112+samcasmmm@users.noreply.github.com>
2024-01-15 19:41:21 +0530

    fix read

--- fs/read.rs
+++ fs/read.rs
@@ -1,12 +1,15 @@
 use std::fs::File;
-use std::io::Read;
+use std::io::{self, Read};
 
-fn main() -> std::io::Result<()> {
-    let mut file = File::open("demo.txt")?;
+fn main() -> io::Result<()> {
+    let mut file = File::open("./src/demo.txt")?;
     let mut contents = Vec::new();
     file.read_to_end(&mut contents)?;
 
-    println!("File contents: {:?}", contents);
+    // Convert the Vec<u8> to a String
+    let contents_string = String::from_utf8_lossy(&contents);
+
+    println!("File contents: {}", contents_string);
 
     Ok(())
 }
