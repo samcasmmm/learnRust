9dd0f1cdea963b36580fd0f8b3c152f3fe0b4a3b
Sameer Bagwan <87710112+samcasmmm@users.noreply.github.com>
2024-01-17 22:02:39 +0530

    insertion_sort

--- dsa/insertion.rs
+++ dsa/insertion.rs
@@ -7,3 +7,18 @@ fn insertion_sort(arr: &mut [T]) {
         }
     }
 }
+
+
+fn main() {
+    println!("Sort numbers ascending");
+    let mut numbers = [4, 65, 2, -31, 0, 99, 2, 83, 782, 1];
+    println!("Before: {:?}", numbers);
+    insertion_sort(&mut numbers);
+    println!("After:  {:?}\n", numbers);
+
+    println!("Sort strings alphabetically");
+    let mut strings = ["beach", "hotel", "airplane", "car", "house", "art"];
+    println!("Before: {:?}", strings);
+    insertion_sort(&mut strings);
+    println!("After:  {:?}\n", strings);
+}
